223a81b7b266ded9a129b0db92feae3c7338c2d2
Andrew <andreiduffy@gmail.com>
2019-01-09 11:30:52 +0300

    1.2

--- index.php
+++ index.php
@@ -1,6 +1,37 @@
 ﻿<?php include('header.php'); ?>
 
-<section class="page">
+<script>
+	function detectIE() {
+		var ua = window.navigator.userAgent;
+		var msie = ua.indexOf('MSIE ');
+		if (msie > 0) {
+			return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+		}
+		var trident = ua.indexOf('Trident/');
+		if (trident > 0) {
+			var rv = ua.indexOf('rv:');
+			return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+		}
+		var edge = ua.indexOf('Edge/');
+		if (edge > 0) {
+			return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+		}
+		return false;
+	}
+
+	var version = detectIE();
+	var pageElement = document.querySelector('body');
+	console.log(pageElement);
+
+	if (version === false) {
+
+	} else {
+		pageElement.classList.add('ie');
+	}
+</script>
+
+
+<section class="page" id="page">
 	<div class="container">
 
 		<div class="logo">
@@ -29,6 +60,9 @@
 
 
 		<div class="form-block">
+			<div class="form-block__border">
+				<img src="/img/border.jpg" alt="border">
+			</div>
 			<div class="form-block__title">Дарим подарки своим будущим клиентам</div>
 
 			<form class="ajax-form">
