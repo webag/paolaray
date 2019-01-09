b40349d0260b5fa409d7f1a4bbc82fb4349a68d4
Andrew <andreiduffy@gmail.com>
2019-01-09 11:53:44 +0300

    1.2

--- header.php
+++ header.php
@@ -27,7 +27,7 @@
 	<meta property="og:image" content="http://<?=$_SERVER['SERVER_NAME']?>/img/og.jpg">
 	<!-- og -->
 
-	<link href="/css/main.css?v=1" rel="stylesheet">
+	<link href="/css/main.css?v=2" rel="stylesheet">
 </head>
 
 <body>
